b511e8e677269a77b64199728cb3310474806d34
Jason Forlinx <forlinx2021@gmail.com>
2025-06-26 14:44:44 +0800

    Auto update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Github\MkDocs\my-doc-site\venv
+set VIRTUAL_ENV=D:\Documents\GitHub\forlinx\forlinx-online-docs\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
